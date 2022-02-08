64d8f1a11e9b6133021de5c71b9f82263d4376a0
yuan.jy <Jason_Yuan@epam.com>
2022-02-08 09:23:34 +0800

    add excel compare

--- Selenium/SeleniumIntegrationTest/IntegrationAutomation/Excel/ExcelHelper.cs
+++ Selenium/SeleniumIntegrationTest/IntegrationAutomation/Excel/ExcelHelper.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace IntegrationAutomation.Excel
@@ -22,7 +24,17 @@ namespace IntegrationAutomation.Excel
                 switch (action.CommandName)
                 {
                     case "Compare File":
-                        var browserName = action.CommandComponent;
+                        var commandType = action.CommandType;
+                        if(commandType == "Last Modified File")
+                        {
+                            string originalFile = action.CommandValue;
+                            string compareFile = GetLastModifiedFile(action.CommandComponent);
+                            bool result = CompareFile(originalFile, compareFile);
+                            if(!result)
+                            {
+                                throw new ArgumentException("Two files are different:\r\n" + originalFile + "\r\n" + compareFile);
+                            }
+                        }
 
                         break;
                     default:
@@ -53,6 +65,50 @@ namespace IntegrationAutomation.Excel
                     throw new ArgumentException("Wait time is not an integer");
                 }
             }
+        }
+
+        public bool CompareDataTable(DataTable originalDataTable, DataTable compareDataTable)
+        {
+            if (originalDataTable.Rows.Count != compareDataTable.Rows.Count || originalDataTable.Columns.Count != compareDataTable.Columns.Count)
+                return false;
+
+            for (int i = 0; i < originalDataTable.Rows.Count; i++)
+            {
+                for (int c = 0; c < originalDataTable.Columns.Count; c++)
+                {
+                    if (!Equals(originalDataTable.Rows[i][c], compareDataTable.Rows[i][c]))
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        private string GetLastModifiedFile(string path)
+        {
+            var files =  new DirectoryInfo(path).GetFiles()
+                        .OrderByDescending(f => f.LastWriteTime)
+                        .Select(f => f.FullName)
+                        .ToList();
+            return files.FirstOrDefault();
+        }
+
+        private bool CompareFile(string originalFile, string compareFile)
+        {
+            ExcelReader reader = new ExcelReader();
+            string originalFileActiveSheetName = "";
+            string compareFileActiveSheetName = "";
+            originalFile = CleanString(originalFile);
+            compareFile = CleanString(compareFile);
+            var dsOriginalFile = reader.ExcelToDataSet(originalFile, "", -1, out originalFileActiveSheetName);
+            var dsCompareFile = reader.ExcelToDataSet(compareFile, "", -1, out compareFileActiveSheetName);
+
+            return CompareDataTable(dsOriginalFile.Tables[0], dsCompareFile.Tables[0]);
+        }
+
+        private string CleanString(string originalString)
+        {
+            var regex = new Regex(@"[\p{Cc}\p{Cf}\p{Mn}\p{Me}\p{Zl}\p{Zp}]");
+            return regex.Replace(originalString, "");
         }
     }
 }
---
+++ Selenium/SeleniumIntegrationTest/IntegrationAutomationTest/ExcelHelperTester.cs
@@ -0,0 +1,109 @@
+﻿using IntegrationAutomation;
+using IntegrationAutomation.Excel;
+using Moq;
+using System.Data;
+using Xunit;
+
+namespace IntegrationAutomationTest
+{
+    public class ExcelHelperTester
+    {
+        [Fact]
+        public void NoColumnsAndNoRowsReturnTrue()
+        {
+            DataTable originalDt = new DataTable();
+            DataTable compareDt = new DataTable();
+
+            Mock<ILog> logService = new Mock<ILog>();
+            ExcelHelper operation = new ExcelHelper(logService.Object);
+            var result = operation.CompareDataTable(originalDt, compareDt);
+            Assert.True(result == true);
+        }
+
+        [Fact]
+        public void SameColumnsAndDifferentRowsReturnFalse()
+        {
+            DataTable originalDt = new DataTable();
+            AddColumns(originalDt, new string[] { "1", "2" });
+            AddRows(originalDt, new string[] { "1,1", "2,2" });
+            DataTable compareDt = new DataTable();
+            AddColumns(compareDt, new string[] { "1", "2" });
+            AddRows(compareDt, new string[] { "1,1", "2,2", "3,3" });
+
+            Mock<ILog> logService = new Mock<ILog>();
+            ExcelHelper operation = new ExcelHelper(logService.Object);
+            var result = operation.CompareDataTable(originalDt, compareDt);
+            Assert.True(result == false);
+        }
+
+        [Fact]
+        public void DifferentColumnsAndSameRowsReturnFalse()
+        {
+            DataTable originalDt = new DataTable();
+            AddColumns(originalDt, new string[] { "1", "2", "3" });
+            AddRows(originalDt, new string[] { "1,1,1", "2,2,2" });
+            DataTable compareDt = new DataTable();
+            AddColumns(compareDt, new string[] { "1", "2" });
+            AddRows(compareDt, new string[] { "1,1", "2,2" });
+
+            Mock<ILog> logService = new Mock<ILog>();
+            ExcelHelper operation = new ExcelHelper(logService.Object);
+            var result = operation.CompareDataTable(originalDt, compareDt);
+            Assert.True(result == false);
+        }
+
+        [Fact]
+        public void DifferentColumnsAndDifferentRowsReturnFalse()
+        {
+            DataTable originalDt = new DataTable();
+            AddColumns(originalDt, new string[] { "1", "2", "3" });
+            AddRows(originalDt, new string[] { "1,1,1", "2,2,2" });
+            DataTable compareDt = new DataTable();
+            AddColumns(compareDt, new string[] { "1", "2" });
+            AddRows(compareDt, new string[] { "1,1", "2,2","3,3" });
+
+            Mock<ILog> logService = new Mock<ILog>();
+            ExcelHelper operation = new ExcelHelper(logService.Object);
+            var result = operation.CompareDataTable(originalDt, compareDt);
+            Assert.True(result == false);
+        }
+
+        [Fact]
+        public void SameColumnsAndSameRowsReturnTrue()
+        {
+            DataTable originalDt = new DataTable();
+            AddColumns(originalDt, new string[] { "1", "2", "3" });
+            AddRows(originalDt, new string[] { "1,1,1", "2,2,2" });
+            DataTable compareDt = new DataTable();
+            AddColumns(compareDt, new string[] { "1", "2", "3" });
+            AddRows(compareDt, new string[] { "1,1,1", "2,2,2" });
+
+            Mock<ILog> logService = new Mock<ILog>();
+            ExcelHelper operation = new ExcelHelper(logService.Object);
+            var result = operation.CompareDataTable(originalDt, compareDt);
+            Assert.True(result == true);
+        }
+
+        private void AddColumns(DataTable dt, string[] columnNames)
+        {
+            if (columnNames != null)
+            {
+                foreach (var columnName in columnNames)
+                {
+                    dt.Columns.Add(columnName);
+                }
+            }
+        }
+
+        private void AddRows(DataTable dt, string[] rowDatas, char splitChar = ',')
+        {
+            if (rowDatas != null)
+            {
+                foreach (var rowData in rowDatas)
+                {
+                    dt.Rows.Add(rowData.Split(splitChar));
+                }
+            }
+        }
+    }
+}
